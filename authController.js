c7b5411bbfbe50b89e46ea0f2f36f3a4397146c3
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-25 21:55:09 +0300

    implement user controller

---
+++ authController.js
@@ -0,0 +1,28 @@
+class authController {
+    async registration(req, res) {
+
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async login(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async getUsers(req, res) {
+        try {
+            res.json({ message: 'Server is working..' })
+        } catch (error) {
+
+        }
+    }
+}
+
+module.exports = new authController()
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
+const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 3010
 
 const app = express()
+app.use(express.json())
+app.use('/auth', authRouter)
 
 const start = async () => {
     try {
