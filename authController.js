295884a00fc5421dc06506fead9a5fa54e7fd1ed
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-25 23:34:12 +0300

    implement user registration

--- authController.js
+++ authController.js
@@ -1,10 +1,27 @@
+const User = require('./models/Users')
+const Role = require('./models/Role')
+const bcrypt = require('bcryptjs')
+
 class authController {
     async registration(req, res) {
-
         try {
+            const {username, password} = req.body
+            const candidate = await User.findOne({username})
+
+            if(candidate) {
+                return res.status(400).json({message: 'User is already exist'})
+            }
+
+            const hashPassword = bcrypt.hashSync(password, 7)
+            const userRole = await Role.findOne({value: 'USER'})
+            const user = new User({username, password: hashPassword, roles: [userRole.value]})
+            await user.save()
+
+            return res.json({message: 'User registered successfully'})
 
         } catch (error) {
-
+            console.log(error)
+            res.status(400).json({message: 'Registration error'})
         }
     }
 
@@ -12,7 +29,8 @@ class authController {
         try {
 
         } catch (error) {
-
+            console.log(error)
+            res.status(400).json({message: 'Login error'})
         }
     }
 
