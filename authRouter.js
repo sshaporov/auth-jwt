afe2f529590dcd03a12f8c08b0a8ed587530dbce
Sergey Shaporov <sshaporov86@gmail.com>
2021-03-26 12:30:28 +0300

    add validator lib and implement validation flow

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const {check} = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+    check('username', 'Username can not be empty').notEmpty(),
+    check('password', 'Password should be more than 4 symbols and less 10').isLength({min: 4, max: 10})
+], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
